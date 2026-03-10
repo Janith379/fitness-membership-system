56c497d856769d31aab2d82377e9d664c31a7802
Janith <janiyayt007@gmail.com.com>
2026-03-10 21:58:37 +0530

    deleted files

--- src/main/java/com/example/fitnesscentersystest1/controller/MemberController.java
+++ src/main/java/com/example/fitnesscentersystest1/controller/MemberController.java
@@ -1,4 +1,42 @@
 package com.example.fitnesscentersystest1.controller;
 
+import com.fitness.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class MemberController {
+
+    MemberService service=new MemberService();
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(){
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerMember(@RequestParam int id,
+                                 @RequestParam String name,
+                                 @RequestParam String plan,
+                                 @RequestParam String duration){
+
+        service.registerMember(id,name,plan,duration);
+
+        return "redirect:/members";
+    }
+
+    @GetMapping("/members")
+    public String viewMembers(Model model){
+
+        model.addAttribute("members",service.getMembers());
+
+        return "members";
+    }
+
 }
